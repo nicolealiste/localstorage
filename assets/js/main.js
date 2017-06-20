5d9c740ac956b491576f055f7b0f138c0eb4f326
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-06-20 18:04:58 -0400

    se finaliza localstorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 //se recuperan los datos que el usuario ingrese en los input
 function guardarDatos(){
-	localStorage.nombre= document.getElementById("nombre").value;
-	localStorage.password= document.getElementById("password").value;
+	localStorage.nombre = document.getElementById("nombre").value;
+	localStorage.password = document.getElementById("password").value;
 }
-//
+//con la siguiente función se imprimiran los datos en pantalla si son completados de lo contrario retornara un alert
+function recuperarDatos(){
+	if((localStorage.nombre != undefined) && (localStorage.password != undefined)){
+		document.getElementById("datos").innerHTML = "Nombre : " + localStorage.nombre + "<br/> Password : " + localStorage.password;		
+	}else{
+		document.getElementById("datos").innerHTML = "No has introducido tu nombre y tu password";
+	}
+}
+
+
